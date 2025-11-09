945c3dfff101da8b568abc4c205c7ab627160023
Debojjo Talukdar <talukdardebojjo26@@gmail.com>
2025-11-09 19:45:39 +0530

    fix github action changes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.use(securityMiddleware);
 
@@ -25,12 +29,18 @@ app.get('/', (req, res) => {
   res.status(200).send('Acquisitions!');
 });
 
-app.get('/health', (req,res) => {
-  res.status(200).json({ status: 'OK', timestamp: new Date().toDateString(), uptime: process.uptime() });
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toDateString(),
+      uptime: process.uptime(),
+    });
 });
 
-app.get('/api', (req,res) => {
-  res.status(200).json({ message: 'Acquisition API is running! '});
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisition API is running! ' });
 });
 
 app.use('/api/auth', authRoutes);
